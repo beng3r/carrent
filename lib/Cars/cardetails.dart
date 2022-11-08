ea5d1f48147a9216edfb9cc9df91e36f9fe662fe
beng3r <luqmankiller85@gmail.com>
2022-11-08 22:44:57 +0800

    updated please +calender, +coupon

--- lib/Cars/cardetails.dart
+++ lib/Cars/cardetails.dart
@@ -5,6 +5,7 @@ import 'car.dart';
 
 class CarDetails extends StatefulWidget {
   final Cars car;
+
   const CarDetails({Key? key, required this.car}) : super(key: key);
 
   @override
@@ -15,24 +16,25 @@ class CarDetails extends StatefulWidget {
 
 class _CarDetailsState extends State<CarDetails> {
   int _sliderVal = 1;
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Color.fromARGB(255, 28, 124, 68),
-        elevation: 0,
-        title: Center(child: Text('C A R R E N T')),
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: Icon(Icons.person),
-          )
-        ],
-      ),
-      drawer: Drawer(),
-      body: SafeArea(
-        child: Column(
-          children: <Widget>[
+        appBar: AppBar(
+          backgroundColor: Color.fromARGB(255, 28, 124, 68),
+          elevation: 0,
+          title: Center(child: Text('C A R R E N T')),
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.person),
+            )
+          ],
+        ),
+        drawer: Drawer(),
+        body: SafeArea(
+          child: Column(children: <Widget>[
             SizedBox(
               height: 200,
               width: double.infinity,
@@ -53,29 +55,95 @@ class _CarDetailsState extends State<CarDetails> {
                 itemBuilder: (BuildContext context, int index) {
                   final specs = widget.car.specs[index];
                   return Text(
+                      '${'TOTAL = RM '} '
                       '${widget.car.dailyRate * _sliderVal} '
-                      '${widget.car.name} ',
+                      '${'for'} '
+                      '${_sliderVal}'
+                      '${'day(s)'}'
+                      '\n'
+                      'Car Details : '
+                      '\n'
+                      '${specs.brand}'
+                      '\n'
+                      '${specs.seats}'
+                      ' seaters',
                       style: const TextStyle(fontSize: 28));
                 },
               ),
             ),
-            Slider(
-              min: 1,
-              max: 10,
-              divisions: 10,
-              label: '${_sliderVal * widget.car.dailyRate} rate',
-              value: _sliderVal.toDouble(),
-              onChanged: (newValue) {
-                setState(() {
-                  _sliderVal = newValue.round();
-                });
-              },
-              activeColor: Colors.green,
-              inactiveColor: Colors.black,
+            SizedBox(
+              height: 10,
             ),
-          ],
-        ),
-      ),
-    );
+            Form(
+              key: _formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  TextFormField(
+                    // The validator receives the text that the user has entered.
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter some text';
+                      }
+                      return null;
+                    },
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 16.0),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        // Validate returns true if the form is valid, or false otherwise.
+                        if (_formKey.currentState!.validate()) {
+                          // If the form is valid, display a snackbar. In the real world,
+                          // you'd often call a server or save the information in a database.
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(content: Text('Processing Data')),
+                          );
+                        }
+                      },
+                      child: const Text('Submit'),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                          color: Colors.grey[200],
+                          border: Border.all(color: Colors.white),
+                          borderRadius: BorderRadius.circular(12)),
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 20.0),
+                        child: TextField(
+                          //controller: _emailcontroller,
+                          decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: 'Discount Code',
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Slider(
+                    min: 1,
+                    max: 30,
+                    divisions: 10,
+                    label: '${_sliderVal} day(s)',
+                    value: _sliderVal.toDouble(),
+                    onChanged: (newValue) {
+                      setState(() {
+                        _sliderVal = newValue.round();
+                      });
+                    },
+                    activeColor: Colors.green,
+                    inactiveColor: Colors.black,
+                  ),
+                ],
+              ),
+            ),
+          ]),
+        ));
   }
 }
--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -1,3 +1,109 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+import '../read data/get_username.dart';
+
+class UserProfile extends StatefulWidget {
+  const UserProfile({super.key});
+
+  @override
+  State<UserProfile> createState() => _UserProfileState();
+}
+
+class _UserProfileState extends State<UserProfile> {
+  final user = FirebaseAuth.instance.currentUser!;
+//IDs
+  List<String> docIDs = [];
+
+//get IDs
+
+  Future getDocId() async {
+    await FirebaseFirestore.instance
+        .collection('users')
+        .get()
+        .then((snapshot) => snapshot.docs.forEach((document) {
+              print(document.reference);
+              docIDs.add(document.reference.id);
+            }));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 28, 124, 68),
+        elevation: 0,
+        title: Center(child: Text('C A R R E N T')),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.person),
+          )
+        ],
+      ),
+      drawer: Drawer(),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('signed in as : ${user.email!}'),
+            MaterialButton(
+              onPressed: () {
+                FirebaseAuth.instance.signOut();
+              },
+              color: Colors.blueGrey,
+              child: Text('sign out'),
+            ),
+            Expanded(
+              child: FutureBuilder(
+                future: getDocId(),
+                builder: (context, snapshot) {
+                  return ListView.builder(
+                      itemCount: docIDs.length,
+                      itemBuilder: (context, index) {
+                        return ListTile(
+                          title: GetUserName(
+                            documentId: docIDs[index],
+                          ),
+                        );
+                      });
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+
+      /* Text('signed in as : ${user.email!}'),
+      MaterialButton(
+        onPressed: () {
+          FirebaseAuth.instance.signOut();
+        },
+        color: Colors.blueGrey,
+        child: Text('sign out'),
+      ),
+      Expanded(
+        child: FutureBuilder(
+          future: getDocId(),
+          builder: (context, snapshot) {
+            return ListView.builder(
+                itemCount: docIDs.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: GetUserName(
+                      documentId: docIDs[index],
+                    ),
+                  );
+                });
+          },
+        ),
+      ), */
+    );
+  }
+}
+
 /*      body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
